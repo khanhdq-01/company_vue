25c3ff21fcbc8d93b87dfbee11a264374e653623
dungnguyen-kma <dungnguyen.kma@gmail.com>
2025-04-24 17:38:36 +0700

    add vue 3 toastiify

--- src/main.js
+++ src/main.js
@@ -2,11 +2,28 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import BootstrapVue3 from "bootstrap-vue-3";
+import Vue3Toastify, { toast } from "vue3-toastify";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
+import "vue3-toastify/dist/index.css";
 
 import "./assets/custom.scss";
+import "./assets/css/style.scss";
+import { createPinia } from "pinia";
 
 const app = createApp(App);
-app.use(BootstrapVue3).use(router).mount("#app");
+const pinia = createPinia()
+
+app.use(pinia)
+app.use(BootstrapVue3)
+   .use(router)
+   .use(Vue3Toastify, {
+     autoClose: 3000,
+     position: toast.POSITION.TOP_RIGHT,
+     theme: "light",
+     hideProgressBar: false,
+     closeOnClick: true,
+     pauseOnHover: true,
+   })
+   .mount("#app");
