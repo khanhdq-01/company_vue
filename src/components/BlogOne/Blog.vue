20aaf96a87ff4bcb28f1676af0a5009dae6aff7e
huynh.ph <huynhphan14112000@gmail.com>
2025-04-28 16:22:47 +0700

    api_blog

--- src/components/BlogOne/Blog.vue
+++ src/components/BlogOne/Blog.vue
@@ -1,357 +1,129 @@
 <template>
     <div class="blog-area ptb-100">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4 col-md-6">
-                    <div class="single-blog-post">
-                        <div class="post-image">
-                            <router-link to="/single-blog">
-                                <img src="../../assets/img/blog-image/1.jpg" alt="image">
-                            </router-link>
-
-                            <div class="date">
-                                <i class='bx bx-calendar'></i> 
-                                Oct 14, 2022
-                            </div>
-                        </div>
-
-                        <div class="post-content">
-                            <h3>
-                                <router-link to="/single-blog">
-                                    50 world-changing people -- We lost in the 2010s
-                                </router-link>
-                            </h3>
-
-                            <div class="post-info">
-                                <div class="post-by">
-                                    <img src="../../assets/img/author-image//1.jpg" alt="image">
-
-                                    <h6>Sarah Taylor</h6>
-                                </div>
-
-                                <div class="details-btn">
-                                    <router-link to="/single-blog">
-                                        <i class="bx bx-right-arrow-alt"></i>
-                                    </router-link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+      <div class="container">
+        <div class="row">
+          <div
+            class="col-lg-4 col-md-6"
+            v-for="blog in blogs"
+            :key="blog.id"
+          >
+            <div class="single-blog-post">
+              <div class="post-image">
+                <div class="date">
+                  <i class="bx bx-calendar"></i>
+                  {{ formatDate(blog.created_at) }}
                 </div>
+              </div>
 
-                <div class="col-lg-4 col-md-6">
-                    <div class="single-blog-post">
-                        <div class="post-image">
-                            <router-link to="/single-blog">
-                                <img src="../../assets/img/blog-image/2.jpg" alt="image">
-                            </router-link>
+              <div class="post-content">
+                <h3>
+                  <router-link :to="`/single-blog/${blog.id}`">
+                    {{ blog.title }}
+                  </router-link>
+                </h3>
 
-                            <div class="date">
-                                <i class='bx bx-calendar'></i> 
-                                Oct 16, 2022
-                            </div>
-                        </div>
+                <div class="post-info">
+                  <div class="post-by">
+                    <img src="../../assets/img/author-image/1.jpg" alt="author" />
+                    <h6>{{ blog.user.name }}</h6>
+                  </div>
 
-                        <div class="post-content">
-                            <h3>
-                                <router-link to="/single-blog">
-                                    Don't buy a tech gift until you read these rules
-                                </router-link>
-                            </h3>
-
-                            <div class="post-info">
-                                <div class="post-by">
-                                    <img src="../../assets/img/author-image//2.jpg" alt="image">
-
-                                    <h6>Michel John</h6>
-                                </div>
-
-                                <div class="details-btn">
-                                    <router-link to="/single-blog">
-                                        <i class="bx bx-right-arrow-alt"></i>
-                                    </router-link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                  <div class="details-btn">
+                    <router-link :to="`/single-blog/${blog.id}`">
+                      <i class="bx bx-right-arrow-alt"></i>
+                    </router-link>
+                  </div>
                 </div>
-
-                <div class="col-lg-4 col-md-6">
-                    <div class="single-blog-post">
-                        <div class="post-image">
-                            <router-link to="/single-blog">
-                                <img src="../../assets/img/blog-image/3.jpg" alt="image">
-                            </router-link>
-
-                            <div class="date">
-                                <i class='bx bx-calendar'></i> 
-                                Oct 18, 2022
-                            </div>
-                        </div>
-
-                        <div class="post-content">
-                            <h3>
-                                <router-link to="/single-blog">
-                                    The golden rule of buying a phone as a gift
-                                </router-link>
-                            </h3>
-
-                            <div class="post-info">
-                                <div class="post-by">
-                                    <img src="../../assets/img/author-image//3.jpg" alt="image">
-
-                                    <h6>Lucy Eva</h6>
-                                </div>
-
-                                <div class="details-btn">
-                                    <router-link to="/single-blog">
-                                        <i class="bx bx-right-arrow-alt"></i>
-                                    </router-link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-4 col-md-6">
-                    <div class="single-blog-post">
-                        <div class="post-image">
-                            <router-link to="/single-blog">
-                                <img src="../../assets/img/blog-image/4.jpg" alt="image">
-                            </router-link>
-
-                            <div class="date">
-                                <i class='bx bx-calendar'></i> 
-                                Oct 14, 2022
-                            </div>
-                        </div>
-
-                        <div class="post-content">
-                            <h3>
-                                <router-link to="/single-blog">
-                                    Configure Redux Into Your WordPress Theme
-                                </router-link>
-                            </h3>
-
-                            <div class="post-info">
-                                <div class="post-by">
-                                    <img src="../../assets/img/author-image//2.jpg" alt="image">
-
-                                    <h6>Sarah Taylor</h6>
-                                </div>
-
-                                <div class="details-btn">
-                                    <router-link to="/single-blog">
-                                        <i class="bx bx-right-arrow-alt"></i>
-                                    </router-link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-4 col-md-6">
-                    <div class="single-blog-post">
-                        <div class="post-image">
-                            <router-link to="/single-blog">
-                                <img src="../../assets/img/blog-image/5.jpg" alt="image">
-                            </router-link>
-
-                            <div class="date">
-                                <i class='bx bx-calendar'></i> 
-                                Oct 16, 2022
-                            </div>
-                        </div>
-
-                        <div class="post-content">
-                            <h3>
-                                <router-link to="/single-blog">
-                                    How To Setup Redux In React Next Application
-                                </router-link>
-                            </h3>
-
-                            <div class="post-info">
-                                <div class="post-by">
-                                    <img src="../../assets/img/author-image//3.jpg" alt="image">
-
-                                    <h6>Michel John</h6>
-                                </div>
-
-                                <div class="details-btn">
-                                    <router-link to="/single-blog">
-                                        <i class="bx bx-right-arrow-alt"></i>
-                                    </router-link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-4 col-md-6">
-                    <div class="single-blog-post">
-                        <div class="post-image">
-                            <router-link to="/single-blog">
-                                <img src="../../assets/img/blog-image/6.jpg" alt="image">
-                            </router-link>
-
-                            <div class="date">
-                                <i class='bx bx-calendar'></i> 
-                                Oct 18, 2022
-                            </div>
-                        </div>
-
-                        <div class="post-content">
-                            <h3>
-                                <router-link to="/single-blog">
-                                    How To Resubmit Rejected Item Into ThemeForest?
-                                </router-link>
-                            </h3>
-
-                            <div class="post-info">
-                                <div class="post-by">
-                                    <img src="../../assets/img/author-image//1.jpg" alt="image">
-
-                                    <h6>Lucy Eva</h6>
-                                </div>
-
-                                <div class="details-btn">
-                                    <router-link to="/single-blog">
-                                        <i class="bx bx-right-arrow-alt"></i>
-                                    </router-link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-4 col-md-6">
-                    <div class="single-blog-post">
-                        <div class="post-image">
-                            <router-link to="/single-blog">
-                                <img src="../../assets/img/blog-image/7.jpg" alt="image">
-                            </router-link>
-
-                            <div class="date">
-                                <i class='bx bx-calendar'></i> 
-                                Oct 14, 2022
-                            </div>
-                        </div>
-
-                        <div class="post-content">
-                            <h3>
-                                <router-link to="/single-blog">
-                                    How To The Active Menu Based On URL In Next.JS?
-                                </router-link>
-                            </h3>
-
-                            <div class="post-info">
-                                <div class="post-by">
-                                    <img src="../../assets/img/author-image//3.jpg" alt="image">
-
-                                    <h6>Sarah Taylor</h6>
-                                </div>
-
-                                <div class="details-btn">
-                                    <router-link to="/single-blog">
-                                        <i class="bx bx-right-arrow-alt"></i>
-                                    </router-link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-4 col-md-6">
-                    <div class="single-blog-post">
-                        <div class="post-image">
-                            <router-link to="/single-blog">
-                                <img src="../../assets/img/blog-image/8.jpg" alt="image">
-                            </router-link>
-
-                            <div class="date">
-                                <i class='bx bx-calendar'></i> 
-                                Oct 16, 2022
-                            </div>
-                        </div>
-
-                        <div class="post-content">
-                            <h3>
-                                <router-link to="/single-blog">
-                                    How To Create A Responsive Popup Gallery?
-                                </router-link>
-                            </h3>
-
-                            <div class="post-info">
-                                <div class="post-by">
-                                    <img src="../../assets/img/author-image//2.jpg" alt="image">
-
-                                    <h6>Michel John</h6>
-                                </div>
-
-                                <div class="details-btn">
-                                    <router-link to="/single-blog">
-                                        <i class="bx bx-right-arrow-alt"></i>
-                                    </router-link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-4 col-md-6">
-                    <div class="single-blog-post">
-                        <div class="post-image">
-                            <router-link to="/single-blog">
-                                <img src="../../assets/img/blog-image/9.jpg" alt="image">
-                            </router-link>
-
-                            <div class="date">
-                                <i class='bx bx-calendar'></i> 
-                                Oct 18, 2022
-                            </div>
-                        </div>
-
-                        <div class="post-content">
-                            <h3>
-                                <router-link to="/single-blog">
-                                    WP Custom Post Register & Query
-                                </router-link>
-                            </h3>
-
-                            <div class="post-info">
-                                <div class="post-by">
-                                    <img src="../../assets/img/author-image//1.jpg" alt="image">
-
-                                    <h6>Lucy Eva</h6>
-                                </div>
-
-                                <div class="details-btn">
-                                    <router-link to="/single-blog">
-                                        <i class="bx bx-right-arrow-alt"></i>
-                                    </router-link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-12 col-md-12">
-                    <div class="pagination-area">
-                        <a href="#" class="prev page-numbers"><i class='bx bxs-arrow-to-left'></i></a>
-                        <a href="#" class="page-numbers">1</a>
-                        <span class="page-numbers current" aria-current="page">2</span>
-                        <a href="#" class="page-numbers">3</a>
-                        <a href="#" class="page-numbers">4</a>
-                        <a href="#" class="next page-numbers"><i class='bx bxs-arrow-to-right'></i></a>
-                    </div>
-                </div>
+              </div>
             </div>
+          </div>
+
+          <div class="col-lg-12 col-md-12">
+            <div class="pagination-area">
+              <button
+                :disabled="!pagination.prev_page_url"
+                @click="fetchBlogs(pagination.current_page - 1)"
+              >
+                &laquo;
+              </button>
+
+              <button
+                v-for="page in pagination.last_page"
+                :key="page"
+                :class="{ active: page === pagination.current_page }"
+                @click="fetchBlogs(page)"
+              >
+                {{ page }}
+              </button>
+
+              <button
+                :disabled="!pagination.next_page_url"
+                @click="fetchBlogs(pagination.current_page + 1)"
+              >
+                &raquo;
+              </button>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-</template>
+  </template>
 
 <script>
-export default {
-    name: 'Blog'
-}
+  import axios from 'axios'
+  import { BASE_API_URL } from "@/main";
+
+  
+  export default {
+    name: 'Blog',
+    data() {
+      return {
+        blogs: [],
+        pagination: {},
+      }
+    },
+    created() {
+      this.fetchBlogs(1)
+    },
+    methods: {
+      async fetchBlogs(page = 1) {
+        try {
+          const token = localStorage.getItem("token");
+          const response = await axios.get(`${BASE_API_URL}/blogs?page=${page}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+          })
+          this.blogs = response.data.data.data
+          this.pagination = {
+            current_page: response.data.data.current_page,
+            last_page: response.data.data.last_page,
+            next_page_url: response.data.data.next_page_url,
+            prev_page_url: response.data.data.prev_page_url,
+          }
+        } catch (error) {
+          console.error('Failed to fetch blogs:', error)
+        }
+      },
+      formatDate(dateStr) {
+        if (!dateStr) return ''
+            const options = { year: 'numeric', month: 'short', day: 'numeric' }
+            return new Date(dateStr).toLocaleDateString(undefined, options)
+        },
+    },
+  }
 </script>
+  
+<style scoped>
+  .pagination-area button {
+    margin: 5px;
+    padding: 5px 10px;
+    border: none;
+    width: 40px;
+  }
+  .pagination-area button.active {
+    background-color: #007bff;
+    color: white;
+  }
+</style>
+  
--- src/router/index.js
+++ src/router/index.js
@@ -171,6 +171,11 @@ const routes = [
       pricingId: route.params.id,
     }),
   },
+  {
+    path: '/single-blog/:id',
+    name: 'BlogDetails',
+    component: () => import('@/components/SingleBlog/BlogDetails.vue')
+  }
 ];
 
 const router = createRouter({
