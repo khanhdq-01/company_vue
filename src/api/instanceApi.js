e2266cbb54d90eba6e88d20175622b2f837db073
dungnguyen-kma <dungnguyen.kma@gmail.com>
2025-04-28 20:38:06 +0700

    re-structure API

--- src/api/instanceApi.js
+++ src/api/instanceApi.js
@@ -1,25 +1,29 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instanceApi = axios.create({
-    baseURL: process.env.VUE_APP_API_URL,
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    }
+  baseURL: process.env.VUE_APP_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+
+export const handleApiError = (error) => {
+  console.log(error.response?.data || error?.message);
+  throw error.response?.data || error?.message;
+};
 
 // Request interceptor
 instanceApi.interceptors.request.use(
-    (config) => {
-        const token = JSON.parse(sessionStorage.getItem('token'));
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+  (config) => {
+    const token = sessionStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
-export default instanceApi; 
+export default instanceApi;
