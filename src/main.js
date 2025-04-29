cee51bca330328f27124d711d9f1f729bf69db19
huynh.ph <huynhphan14112000@gmail.com>
2025-04-29 11:36:53 +0700

    api_blog

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import BootstrapVue3 from "bootstrap-vue-3";
+import CKEditor from '@ckeditor/ckeditor5-vue';
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
@@ -14,3 +15,4 @@ const BASE_IMAGE_URL = "http://127.0.0.1:8000/storage/";
 export { BASE_API_URL, BASE_IMAGE_URL };
 const app = createApp(App);
 app.use(BootstrapVue3).use(router).mount("#app");
+app.use(CKEditor);
