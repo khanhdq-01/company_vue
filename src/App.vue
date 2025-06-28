0ad0c4bcb21938816b1ee77fec7510a7522314df
khanhdq-01 <quockhanhvip98@gmail.com>
2025-06-28 18:41:21 +0700

    fix: add vite1

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
-    <div>
-        <Preloader v-if="isLoading" />
-        <router-view v-slot="{ Component }">
-            <transition 
-                enter-active-class="animated fadeIn" 
-                leave-active-class="animated fadeOut" 
-                mode="out-in" 
-                :duration="600" 
-                @after-leave="$root.$emit('triggerScroll')"
-            >
-                <component :is="Component" />
-            </transition>
+  <div>
+    <Preloader v-if="isLoading" />
+    <router-view v-slot="{ Component }">
+      <transition
+        enter-active-class="animated fadeIn"
+        leave-active-class="animated fadeOut"
+        mode="out-in"
+        :duration="600"
+        @after-leave="$root.$emit('triggerScroll')"
+      >
+        <component :is="Component" />
+      </transition>
     </router-view>
-        <GoTop />
-    </div>
+    <GoTop />
+  </div>
 </template>
 
 <script>
@@ -21,20 +21,24 @@ import Preloader from './components/Layouts/Preloader'
 import GoTop from './components/Layouts/GoTop'
 
 export default {
-    name: 'App',
-    components: {
-        Preloader,
-        GoTop,
-    },
-    data() {
-        return {
-            isLoading: true
-        }
-    },
-    mounted() {
-        setTimeout(() => {
-            this.isLoading = false
-        }, 2000)
+  name: 'App',
+  components: {
+    Preloader,
+    GoTop,
+  },
+  data() {
+    return {
+      isLoading: true,
     }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.isLoading = false
+    }, 2000)
+  },
 }
 </script>
+
+<style scoped>
+/* Bạn có thể thêm style nếu cần */
+</style>
